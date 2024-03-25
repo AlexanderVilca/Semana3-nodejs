af76d5be625f4ac5fcca38c55012b0b79d1a93a4
Mac10 <alumno@MacBook-Pro-10.local>
2024-03-25 11:50:00 -0500

    commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const port = 3500
+const app = express()
+
+app.get('/hello/:username', (req,res) => {
+    console.log(typeof req.params.username)
+    res.send(`Hello ${req.params.username.toUpperCase()}`)
+})
+
+app.get('/add/:x/:y', (req,res) => {
+    const {x, y} = req.params
+    res.send(`Result: ${ parseInt(x) + parseInt(y)}`)
+})
+
+app.get('/users/:username/photo', (req,res) => {
+    if (req.params.username.toUpperCase() === "ALEX") {
+        return res.sendFile('./img/software.webp', {
+            root: __dirname
+        })
+    }
+    res.send('El usuario no tiene acceso......!!!')
+})
+
+app.get('/name/:name/age/:age' , (req, res)=> {
+    res.send(`El usuario ${req.params.name} tiene ${req.params.age} de edad.....!!!`)
+})
+
+app.listen(port)
+console.log('Server on port ${port}')
